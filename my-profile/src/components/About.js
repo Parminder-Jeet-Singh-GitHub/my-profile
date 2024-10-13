cb04a751d152a51bac40d4d84bdba89c04b5ac0e
Vinny <parminderjeetsingh198@gmail.com>
2024-10-13 05:38:11 +0530

    basic profile

---
+++ my-profile/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+      I'm a backend engineer with three years of experience specializing in database management and building robust server-side applications. My preferred languages are Python and Golang, and I have hands-on experience with PostgreSQL for relational databases, MongoDB for non-relational databases, and MinIO for object storage. Additionally, I've worked with AWS and GCP services for API creation and database storage solutions. On the frontend, I have strong proficiency in React.js for web development
+      </p>
+    </section>
+  );
+};
+
+export default About;
