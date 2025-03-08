94c46bb9a4800f49ecceb1c6e9590a9bb7dea97e
Vinny <parminderjeetsingh198@gmail.com>
2025-03-09 01:03:53 +0530

    about and projects added

--- src/App.js
+++ src/App.js
@@ -144,7 +144,7 @@ function App() {
         </header>
 
         <Routes>
-          <Route path="/about" element={<About />} />
+          <Route path="/my-profile" element={<About />} />
           <Route path="/experience" element={<Experience />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
